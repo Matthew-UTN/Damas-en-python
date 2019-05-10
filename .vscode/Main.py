2be37e7a5d6a17ed456919d43128de192de3073f
Matias Ramos <matias.ramos@alumnos.frm.utn.edu.ar>
2019-05-10 12:37:32 -0300

    Borre una carpeta y ordene los archivos

--- .vscode/Main.py
+++
@@ -1,9 +0,0 @@
-from Tablero import *
-
-ancho = 9
-largo = 9
-primerjugador = 0
-
-b = Tablero(ancho, largo, primerjugador)
-b.printTablero()
-print("Welcome to checkers.")
---
+++ Main.py
@@ -0,0 +1,19 @@
+from Tablero import *
+
+ancho = 0
+tmp = 0
+largo = 0
+primerjugador = 0
+
+
+print("Bienvenido")
+while True:
+  tmp=int(input("Selecciona el tamaño del tablero entre 6 y 10."))
+  if (tmp>5):
+    if(tmp<11):
+      break
+
+ancho = tmp
+largo = tmp
+b = Tablero(ancho, largo, primerjugador)
+b.printTablero()
